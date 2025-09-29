04995a3b0a50239c7d859079eda50432d99c35ff
duarteeigor <duarteigor562@gmail.com>
2025-09-29 12:33:19 -0300

    feat: add toaster

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,12 @@ import { SessionProvider } from "next-auth/react"
 import Header from "@/components/header";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
+      <Toaster position="top-center" />
       <Header />
       <Component {...pageProps} />
     </SessionProvider>
